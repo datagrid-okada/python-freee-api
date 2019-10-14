2c5c449d3f984dabc798ba9b282a4bc0a4a20b78
Yuki Okada <yuki.okada@datagrid.co.jp>
2019-10-15 00:46:19 +0900

    Version 0.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requires = ["requests>=2.19.1"]
 
 setup(
     name='python-freee-api',
-    version='0.4',
+    version='0.5',
     description='Python based freee api wrapper',
     url='',
     author='Yuki Okada',
