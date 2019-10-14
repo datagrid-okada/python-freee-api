f7f675d69e10d95e5836c69d62213013c0858f8d
Yuki Okada <yuki.okada@datagrid.co.jp>
2019-10-14 18:39:28 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+requires = ["requests>=2.19.1"]
+
+
+setup(
+    name='python-freee-api',
+    version='0.1',
+    description='Python based freee api wrapper',
+    url='',
+    author='Yuki Okada',
+    author_email='yuki.okada.1993@gmail.com',
+    license='MIT',
+    keywords='python freee api wrapper',
+    packages=find_packages(),
+    install_requires=requires,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+)
